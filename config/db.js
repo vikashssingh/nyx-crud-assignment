473568afdecb7052d565ba5e83c8a381854ce32e
vikashssingh <vikashsingS5530@gmail.com>
2022-08-10 02:22:57 +0530

    change

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@
 // To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
 
 module.exports = {
-  db: process.env.DB
+  db: "mongodb+srv://vikash:vikash@123@cluster0.9oy783x.mongodb.net/?retryWrites=true&w=majority"
 };
